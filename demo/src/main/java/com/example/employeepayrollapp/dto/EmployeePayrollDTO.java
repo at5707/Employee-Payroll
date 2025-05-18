e253238207f98d3d07d2dae50ee7daff40a426c4
at5707 <at5707@srmist.edu.in>
2025-05-18 11:30:00 +0530

    Welcome to EmployeePayrollApp

---
+++ demo/src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,15 @@
+package com.example.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+	private String name;
+	private long salary;
+	public EmployeePayrollDTO() {}
+	public EmployeePayrollDTO(String name,long salary) {
+		this.name=name;
+		this.salary=salary;
+	}
+	public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public long getSalary() { return salary; }
+    public void setSalary(long salary) { this.salary = salary; }
+}
